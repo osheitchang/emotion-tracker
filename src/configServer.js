23dbab5dcefd7396fb34c92c85b63f68271fa5b2
Osheit <chang.osheit@gmail.com>
2019-10-16 16:38:27 -0400

    removed ubi

--- src/configServer.js
+++ src/configServer.js
@@ -2,6 +2,6 @@ let serverUrl = ''
 if (process.env.NODE_ENV === 'production') {
   serverUrl = '' //This is the live version
 } else {
-  serverUrl = 'http://localhost:5000'
+  //serverUrl = 'http://localhost:5000'
 }
 module.exports = serverUrl
