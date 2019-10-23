586aa74007566620922fcc854e302b7ee1ba8087
Osheit <chang.osheit@gmail.com>
2019-10-22 23:28:06 -0400

    new component

---
+++ frontend/src/Components/Graph.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Graph extends Component {
+    render() {
+        return (
+            <div>
+
+            <h3>Hello World!</h3>  
+              
+            </div>
+        );
+    }
+}
+
+export default Graph;
