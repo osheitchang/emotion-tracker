2e52795ae414c20cae7c59f648a33531bf26b3f9
Osheit <chang.osheit@gmail.com>
2019-11-11 22:20:38 -0500

    s

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import serverUrl from '../configServer.js';
 import axios from 'axios';
+import serverUrl from '../configServer.js'
 
 
 class Dashboard extends Component {
